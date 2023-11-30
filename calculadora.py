98bf804fe39b4d864f140b8bb6be9276ccc2d546
sarah <ramossarah462@gmail.com>
2023-11-30 09:21:09 -0300

    realizado de operacao de somas

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,7 @@
 print('Vamos calcular !')
-primeiro= input ('Primeiro numero:')
-Segundo= input('Segundo numero:')
+primeiro= int(float ('Primeiro numero:'))
+Segundo= int(float('Segundo numero:'))
 
 resultado= primeiro + Segundo 
 
+print(resultado)
